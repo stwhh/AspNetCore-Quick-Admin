056ac9d5c3fd732484b7d072539851c7bd71f9d6
Thomson Sun <tnsn@mm-software.com>
2020-08-04 08:34:07 +0800

    feat:ValuesController api作为测试api，不需要Authorize

--- QuickAdmin/QuickAdmin/Controllers/ValuesController.cs
+++ QuickAdmin/QuickAdmin/Controllers/ValuesController.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace QuickAdmin.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
